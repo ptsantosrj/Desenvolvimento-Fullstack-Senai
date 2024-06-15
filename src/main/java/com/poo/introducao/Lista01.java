dc05c5a2ee6e8e3d6e640d657b36352454299471
Patrick Tavares dos Santos <ptsantosrj@gmail.com>
2024-06-14 21:55:37 -0300

    Adicionado ao commit o exercicio 2 da Lista01

--- src/main/java/com/poo/introducao/Lista01.java
+++ src/main/java/com/poo/introducao/Lista01.java
@@ -27,5 +27,29 @@ public class Lista01 {
         scanner.close();
     }
 
+    public static void ex2(String[] args) {
+        // Cria um Scanner para obter entrada do usuário
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicita e obtém o nome
+        System.out.print("Digite o seu nome: ");
+        String nome = scanner.nextLine();
+
+        // Solicita e obtém o sobrenome
+        System.out.print("Digite o sobrenome: ");
+        String sobrenome = scanner.nextLine();
+
+        // Coloca em uma variavel o nome e sobrenome
+        String nomesobrenome = nome + " " + sobrenome;
+
+        // Exibe o resultado
+        System.out.println("Seja bem vindo: " + nomesobrenome);
+
+        // Fecha o Scanner
+        scanner.close();
+    } 
+
+
+
 
 }
