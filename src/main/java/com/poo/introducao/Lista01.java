09cb82bc012689b950d74993ec77cd55727cc413
Patrick Tavares dos Santos <ptsantosrj@gmail.com>
2024-06-14 22:23:24 -0300

    Adicionado ao commit o exercicio 5 da Lista01

--- src/main/java/com/poo/introducao/Lista01.java
+++ src/main/java/com/poo/introducao/Lista01.java
@@ -99,5 +99,45 @@ public class Lista01 {
         scanner.close();
     }
 
+    public static void ex5(String[] args) {
+
+        double larguraTerreno;
+        double comprimentoTerreno;
+        double area;
+        double valorDoM2;
+        double valorTerreno;
+
+        // Cria um Scanner para obter entrada do usuário
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicita e obtém a largura do terreno
+        System.out.print("Digite a largura do terreno: \n");
+        larguraTerreno = scanner.nextDouble();
+
+        // Solicita e obtém o comprimento do terreno
+        System.out.print("Digite o comprimento do terreno: \n");
+        comprimentoTerreno = scanner.nextDouble();
+
+        // Solicita e obtém o valor do m2
+        System.out.print("Digite o valor do m2 em reais: \n");
+        valorDoM2 = scanner.nextDouble();
+
+        // Calculando a area do terreno
+        area = larguraTerreno * comprimentoTerreno;
+
+        // Calculando o valor do terreno
+        valorTerreno = valorDoM2 * area;
+
+        // Arredonda o valor para ate 2 casas decimais
+        // BigDecimal class fornece operações em números duplos para aritmética, manipulação de escala, arredondamento, comparação, conversão de formato e hashing.
+        // HALF_EVEN indica que o arredondamento acontece para o valor mais próximo
+        BigDecimal bd = new BigDecimal(valorTerreno).setScale(3, RoundingMode.HALF_EVEN);
+        
+        // Exibe o resultado
+        System.out.println("O valor do terreno é: R$ " + bd.doubleValue());
+
+        // Fecha o Scanner
+        scanner.close();
+    }
 
 }
