bbd0d348101b2568f2177f245112229c7e50c3d3
Patrick Tavares dos Santos <ptsantosrj@gmail.com>
2024-06-14 21:58:19 -0300

    Adicionado ao commit o exercicio 3 da Lista01

--- src/main/java/com/poo/introducao/Lista01.java
+++ src/main/java/com/poo/introducao/Lista01.java
@@ -1,5 +1,7 @@
 package com.poo.introducao;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class Lista01 {
@@ -49,7 +51,34 @@ public class Lista01 {
         scanner.close();
     } 
 
+    public static void ex3(String[] args) {
+        // Cria um Scanner para obter entrada do usuário
+        Scanner scanner = new Scanner(System.in);
 
+        // Solicita e obtém o valor em reais
+        System.out.print("Digite um valor em reais para converter em dolar: \n ");
+        double valorDigitadoEmReais = scanner.nextDouble();
+
+        // Solicita e obtém a taxa de conversao
+        System.out.print("Digite o valor da taxa de conversao: \n ");
+        double TaxaConversao = scanner.nextDouble();
+        
+        // Calcula a divisao dos dois valores
+        double valorConvertidoParaDolar = valorDigitadoEmReais / TaxaConversao;
+
+        // Arredonda o valor para ate 2 casas decimais
+        // BigDecimal class fornece operações em números duplos para aritmética, manipulação de escala, arredondamento, comparação, conversão de formato e hashing.
+        // HALF_EVEN indica que o arredondamento acontece para o valor mais próximo
+        BigDecimal bd = new BigDecimal(valorConvertidoParaDolar).setScale(2, RoundingMode.HALF_EVEN);
+        
+        // Exibe o resultado
+        System.out.println("O valor em dolar é: $" + bd.doubleValue());
+
+        
+        // System.out.println("O valor em dolar é: $" + Math.round(valorConvertidoParaDolar));
+        // Fecha o Scanner100
+        scanner.close();
+    }
 
 
 }
