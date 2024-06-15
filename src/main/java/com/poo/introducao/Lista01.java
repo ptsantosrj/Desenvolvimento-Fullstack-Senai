01eeb82f6f73c300825534c60a0d658ac630189e
Patrick Tavares dos Santos <ptsantosrj@gmail.com>
2024-06-14 22:24:17 -0300

    Adicionado ao commit o exercicio 6 da Lista01

--- src/main/java/com/poo/introducao/Lista01.java
+++ src/main/java/com/poo/introducao/Lista01.java
@@ -140,4 +140,28 @@ public class Lista01 {
         scanner.close();
     }
 
+    public static void ex6(String[] args) {
+
+        double distanciaKM;
+        double consumoL;
+        double custoL = 5.00;
+
+        // Cria um Scanner para obter entrada do usuário
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicita e obtém a distancia percorrida em km
+        System.out.print("Digite a distancia percorrida em KM: \n");
+        distanciaKM = scanner.nextDouble();
+
+        // Solicita e obtém o gasto de combustivel em Litros
+        System.out.print("Digite o total gasto em combustivel em L: \n");
+        consumoL = scanner.nextDouble();
+
+        System.out.print("Sua autonomia foi de: " + ( distanciaKM/(consumoL/custoL) ) + " Km/L.");
+
+        // Fecha o Scanner
+        scanner.close();
+    }
+
+
 }
