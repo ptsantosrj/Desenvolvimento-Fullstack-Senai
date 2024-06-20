88dc0758217cc1e73887d1763918f351097820a5
Patrick Tavares dos Santos <ptsantosrj@gmail.com>
2024-06-20 20:22:56 -0300

    Adicionado ao commit o exercicio 7 da Lista03

--- src/main/java/com/poo/introducao/Lista03.java
+++ src/main/java/com/poo/introducao/Lista03.java
@@ -195,5 +195,23 @@ public class Lista03 {
 
     }
 
-    
+    public static void ex7(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a largura do retângulo: ");
+        int largura = scanner.nextInt();
+
+        System.out.print("Digite a altura do retângulo: ");
+        int altura = scanner.nextInt();
+
+        for (int i = 1; i <= altura; i++) {
+            for (int j = 1; j <= largura; j++) {
+                System.out.print("*");
+            }
+            System.out.println(); 
+        }
+
+        scanner.close();
+    }
 }
